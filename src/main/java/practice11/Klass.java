14d307a2dbe00e86e666e31a19b1a3b6c5832553
realyxf <997915421@qq.com>
2020-07-22 23:41:36 +0800

    [Fennie] test:finished practice 11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,55 @@
 package practice11;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    private Teacher teacher;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Klass(int number,Teacher teacher) {
+        this.number = number;
+        this.teacher = teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + getNumber();
+    }
+
+    public void assignLeader(Student student) {
+        if(student.getKlass().getNumber() == this.getNumber()){
+            this.leader = student;
+            System.out.print("I am Tom. I know " + student.getName() +" become Leader of Class " + this.number +".\n");
+        }
+        else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        student.getKlass().setNumber(this.number);
+        System.out.print("I am Tom. I know " + student.getName() +" has joined Class " + this.number +".\n");
+    }
 }
--- src/main/java/practice11/Student.java
+++ src/main/java/practice11/Student.java
@@ -1,4 +1,24 @@
 package practice11;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int i, String name, int age, Klass klass) {
+        super(i,name,age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if(getKlass().getLeader() == this){
+            return "My name is "+ getName() + ". I am " + getAge() + " years old. I am a Student. I am Leader of " + getKlass().getDisplayName() + '.';
+        }
+        else {
+            return "My name is "+ getName() + ". I am " + getAge() + " years old. I am a Student. I am at " + getKlass().getDisplayName() + '.';
+        }
+
+    }
 }
