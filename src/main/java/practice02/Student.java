159bd04b031bc464cb22e7437151500df466e008
FENNIE X F YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGFE3@OOCL.COM>
2020-07-22 18:06:41 +0800

    [Fennie] test:finished practice 1 and practice 2

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,21 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    //("I am a Student. I am at Class 2.");
+    public String introduce() {
+        String introduceModel="I am a Student. I am at Class " + this.klass + ".";
+        return introduceModel;
+    }
 }
