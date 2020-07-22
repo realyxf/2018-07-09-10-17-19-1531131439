4fce4f3fae0d26091e1ae811516a5c25ab0b51ae
realyxf <997915421@qq.com>
2020-07-22 22:24:14 +0800

    [Fennie] test:finished practice 7 and practice 8

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,37 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+    private Student student;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce() {
+        if(getKlass()!=null) {
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach " + getKlass().getDisplayName() + '.';
+        }
+        else{
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach No Class.";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if(student.getKlass().getNumber() == this.getKlass().getNumber()){
+            return "My name is " + getName() +  ". I am " + getAge() + " years old. I am a Teacher. I teach " + student.getName() + '.';
+        }
+        else{
+            return "My name is " + getName() +  ". I am " + getAge() + " years old. I am a Teacher. I don't teach " + student.getName() + '.';
+        }
+    }
 }
