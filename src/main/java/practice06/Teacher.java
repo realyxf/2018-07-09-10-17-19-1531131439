c0ca9284f3319c73b08a51effd6857486775b310
realyxf <997915421@qq.com>
2020-07-22 21:17:01 +0800

    [Fennie] test:finished practice 5 and practice 6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,24 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+    public Teacher(String name, int age,int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if(getKlass()>0){
+            return "My name is "+ getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach Class " + getKlass() + '.';
+        }
+        else{
+            return "My name is "+ getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach No Class.";
+        }
+    }
 }
